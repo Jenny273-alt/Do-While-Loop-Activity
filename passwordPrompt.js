873f5c5bf4da67d2445b05e500ff1fd7a1c5aed6
Jenny273-alt <naungayanjenny90@gmail.com>
2024-10-03 10:17:55 +0700

    passwordPrompt.js

---
+++ passwordPrompt.js
@@ -0,0 +1,18 @@
+// Set the correct password
+const correctPassword = "June_202004."; 
+
+let password = "";
+
+// Keep asking for the password until it's correct
+do {
+  password = prompt("Enter your password:");
+
+  if (password === correctPassword) {
+    console.log("Access granted.");
+  } else {
+    console.log("Incorrect password. Try again.");
+  }
+} while (password !== correctPassword);
+
+//https://l.facebook.com/l.php?u=https%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2Fdocs%2FWeb%2FJavaScript%2FReference%2FStatements%2Fdo...while%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR2r5D734MmXHdMPYschJtoqzo9pxGQlzHHjYSWJCufSefGV0ukANgR-e_g_aem_oS6Iv5E9fbKnUwSigFwcFw&h=AT2xCctkMWnkFVePGDbdDI68qIKYVqGlYQRrtrM914zDhbthXGeupfHLh-Rv50u2LksU3knMEMIC_0y9RJAqc6MAYnEqvgie8bAQFY4LgXvfGOhJH0Gl-IPDoZ5r69-J8TZMRA
+//https://l.facebook.com/l.php?u=https%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2Fdocs%2FWeb%2FAPI%2FWindow%2Fprompt%3Ffbclid%3DIwZXh0bgNhZW0CMTAAAR1NSJOfXB_8PlOaQktYFkhhSRPWGdnR2IIOBZsH11ekZ4qGBNgJYZk_6sg_aem_kUhIkaH4wd-YUFYh1t74aA&h=AT2xCctkMWnkFVePGDbdDI68qIKYVqGlYQRrtrM914zDhbthXGeupfHLh-Rv50u2LksU3knMEMIC_0y9RJAqc6MAYnEqvgie8bAQFY4LgXvfGOhJH0Gl-IPDoZ5r69-J8TZMRA
